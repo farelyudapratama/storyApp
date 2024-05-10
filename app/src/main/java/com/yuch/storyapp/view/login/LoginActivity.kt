4b57abf62d2b3175dccc3bff700a7aaabb8b0cd9
farelyudapratama <dhakayudha2@gmail.com>
2024-05-10 10:57:39 +0700

    Login dan Register berhasil terhubung API

--- app/src/main/java/com/yuch/storyapp/view/login/LoginActivity.kt
+++ app/src/main/java/com/yuch/storyapp/view/login/LoginActivity.kt
@@ -8,7 +8,8 @@ import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.yuch.storyapp.data.pref.UserModel
+import com.yuch.storyapp.data.ResultState
+import com.yuch.storyapp.data.response.LoginResponse
 import com.yuch.storyapp.databinding.ActivityLoginBinding
 import com.yuch.storyapp.view.ViewModelFactory
 import com.yuch.storyapp.view.main.MainActivity
@@ -45,23 +46,51 @@ class LoginActivity : AppCompatActivity() {
     private fun setupAction() {
         binding.loginButton.setOnClickListener {
             val email = binding.edLoginEmail.text.toString()
-            viewModel.saveSession(UserModel(email, "sample_token"))
-            AlertDialog.Builder(this).apply {
-                setTitle("Yeah!")
-                setMessage("Anda berhasil login. Sudah tidak sabar untuk belajar ya?")
-                setPositiveButton("Lanjut") { _, _ ->
-                    val intent = Intent(context, MainActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
-                    finish()
+            val password = binding.edLoginPassword.text.toString()
+            viewModel.login(email, password)
+//            viewModel.saveSession(UserModel(email, "sample_token"))
+            viewModel.loginResult.observe(this) { result ->
+                when (result){
+                    is ResultState.Loading -> {
+                        showLoading(true)
+                    }
+                    is ResultState.Success -> {
+                        showLoading(false)
+                        val response: LoginResponse = result.data
+                        AlertDialog.Builder(this).apply {
+                            setTitle("Yeah!")
+                            setMessage("Anda berhasil login sebagai ${response.loginResult?.name}")
+                            setPositiveButton("Lanjut") { _, _ ->
+                                val intent = Intent(context, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                                startActivity(intent)
+                                finish()
+                            }
+                            create()
+                            show()
+                        }
+                    }
+                    is ResultState.Error -> {
+                        showLoading(false)
+                        AlertDialog.Builder(this).apply {
+                            setTitle("Oops...")
+                            setMessage(result.error)
+                            setPositiveButton("Coba Lagi") { _, _ ->
+                            }
+                            create()
+                            show()
+                        }
+                    }
                 }
-                create()
-                show()
             }
         }
+
         binding.signupButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun showLoading(isLoading: Boolean) {
+        binding.pbLogin.visibility = if (isLoading) android.view.View.VISIBLE else android.view.View.GONE
     }
 }
